8432bf76877e6f861e8bc5f57100bb0d568b4c23
vince radlicz <vradlicz6377@stu.d214.org>
2024-08-15 09:19:10 -0500

    Initial Commit

---
+++ AllAboutMe/AllAboutMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAboutMeApp.swift
+//  AllAboutMe
+//
+//  Created by Vincent M. Radlicz on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct AllAboutMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
